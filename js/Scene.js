c107df8bb290241c7160293982a6fe70d1435fd9
krupinskij <jk.krupinski@gmail.com>
2020-02-10 12:53:49 +0100

    Add flowers in random places

--- js/Scene.js
+++ js/Scene.js
@@ -9,10 +9,105 @@ class Scene {
         .then(resp => resp.json())
         .then(model => {
             model.normals = utils.calculateNormals(model.vertices, model.indices);
-            console.log(model);
             this.objects.push(model);
-            //console.log(this.objects);
+            console.info("Added: " + model.partname + " of " + model.alias)
         })
+    }
+
+    loadBumblebee() {
+        const headPromise = scene.loadModel("model/bumblebee/head.json");
+        const bellyPromise = scene.loadModel("model/bumblebee/belly.json");
+        const leftEyePromise = scene.loadModel("model/bumblebee/left-eye.json");
+        const rightEyePromise = scene.loadModel("model/bumblebee/right-eye.json");
+
+        return Promise.all([headPromise, bellyPromise, leftEyePromise, rightEyePromise]);
+    }
+
+    loadGrass() {
+        return scene.loadModel("model/grass/grass.json");
+    }
+
+    loadFlower() {
+        const xPos = Math.random() * 80 - 40;
+        const yPos = Math.random() * 80 - 40;
+
+        const colors = [[0.8, 0.0, 0.0, 1.0], [1.0, 1.0, 1.0, 1.0], [1.0, 0.5, 1.0, 1.0], [1.0, 0.5, 0.2, 1.0], [0.5, 0.5, 1.0, 1.0]];
+        const randomCol = Math.floor(Math.random() * 5)
+
+        const petalPromise = fetch("http://" + document.domain + ":" + location.port + "/model/flower/petal.json")
+        .then(resp => resp.json())
+        .then(model => {
+            model.vertices = model.vertices.map((v, i) => {
+                if(i%3===0) return v + xPos;
+                else if(i%3===2) return v + yPos;
+                else return v;
+            });
+            model.normals = utils.calculateNormals(model.vertices, model.indices);
+            model.color = colors[randomCol];
+            this.objects.push(model)
+            console.info("Added: " + model.partname + " of " + model.alias)
+        })
+
+        const centerPromise = fetch("http://" + document.domain + ":" + location.port + "/model/flower/center.json")
+        .then(resp => resp.json())
+        .then(model => {
+            model.vertices = model.vertices.map((v, i) => {
+                if(i%3===0) return v + xPos;
+                else if(i%3===2) return v + yPos;
+                else return v;
+            });
+            model.normals = utils.calculateNormals(model.vertices, model.indices);
+            this.objects.push(model)
+            console.info("Added: " + model.partname + " of " + model.alias)
+        })
+
+        const stemPromise = fetch("http://" + document.domain + ":" + location.port + "/model/flower/stem.json")
+        .then(resp => resp.json())
+        .then(model => {
+            model.vertices = model.vertices.map((v, i) => {
+                if(i%3===0) return v + xPos;
+                else if(i%3===2) return v + yPos;
+                else return v;
+            });
+            model.normals = utils.calculateNormals(model.vertices, model.indices);
+            this.objects.push(model)
+            console.info("Added: " + model.partname + " of " + model.alias)
+        })
+
+        return Promise.all([petalPromise, centerPromise, stemPromise]);
+    }
+
+    loadDandelion() {
+        const xPos = Math.random() * 80 - 40;
+        const yPos = Math.random() * 80 - 40;
+
+        const centerPromise = fetch("http://" + document.domain + ":" + location.port + "/model/dandelion/center.json")
+        .then(resp => resp.json())
+        .then(model => {
+            model.vertices = model.vertices.map((v, i) => {
+                if(i%3===0) return v + xPos;
+                else if(i%3===2) return v + yPos;
+                else return v;
+            });
+            model.normals = utils.calculateNormals(model.vertices, model.indices);
+            this.objects.push(model)
+            console.info("Added: " + model.partname + " of " + model.alias)
+        })
+
+        const stemPromise = fetch("http://" + document.domain + ":" + location.port + "/model/dandelion/stem.json")
+        .then(resp => resp.json())
+        .then(model => {
+            model.vertices = model.vertices.map((v, i) => {
+                if(i%3===0) return v + xPos;
+                else if(i%3===2) return v + yPos;
+                else return v;
+            });
+            model.normals = utils.calculateNormals(model.vertices, model.indices);
+            this.objects.push(model)
+            console.info("Added: " + model.partname + " of " + model.alias)
+        })
+
+        return Promise.all([centerPromise, stemPromise]);
     }
 
     addLocalModel(model) {
--- js/WebGLApp.js
+++ js/WebGLApp.js
@@ -7,46 +7,19 @@ class WebGLApp {
     run() {
         program.gl = utils.getGLContext("canvas-element-id");
         
-        const headPromise = scene.loadModel("model/bumblebee/head.json");
-        const bellyPromise = scene.loadModel("model/bumblebee/belly.json");
-        const leftEyePromise = scene.loadModel("model/bumblebee/left-eye.json");
-        const rightEyePromise = scene.loadModel("model/bumblebee/right-eye.json");
+        const bumblebeePromise = scene.loadBumblebee();
 
-        const grassPromise = scene.loadModel("model/grass.json");
+        const grassPromise = scene.loadGrass();
 
-        const stem1Promise = scene.loadModel("model/flower1/stem.json");
-        const center1Promise = scene.loadModel("model/flower1/center.json");
-        const petal1Promise = scene.loadModel("model/flower1/petal.json");
+        for(let i = 0; i < 5; i++) {
+            scene.loadFlower();
+        }
 
-        const stem2Promise = scene.loadModel("model/flower2/stem.json");
-        const center2Promise = scene.loadModel("model/flower2/center.json");
-        const petal2Promise = scene.loadModel("model/flower2/petal.json");
+        for(let i = 0; i < 2; i++) {
+            scene.loadDandelion();
+        }
 
-        const stem3Promise = scene.loadModel("model/flower3/stem.json");
-        const center3Promise = scene.loadModel("model/flower3/center.json");
-        const petal3Promise = scene.loadModel("model/flower3/petal.json");
-
-        const stem4Promise = scene.loadModel("model/flower4/stem.json");
-        const center4Promise = scene.loadModel("model/flower4/center.json");
-        const petal4Promise = scene.loadModel("model/flower4/petal.json");
-
-        const stem5Promise = scene.loadModel("model/flower5/stem.json");
-        const center5Promise = scene.loadModel("model/flower5/center.json");
-        const petal5Promise = scene.loadModel("model/flower5/petal.json");
-
-        const stemDPromise = scene.loadModel("model/dandelion/stem.json");
-        const centerDPromise = scene.loadModel("model/dandelion/center.json");
-
-        Promise.all([
-            headPromise, bellyPromise, leftEyePromise, rightEyePromise, 
-            grassPromise, 
-            stem1Promise, center1Promise, petal1Promise,
-            stem2Promise, center2Promise, petal2Promise,
-            stem3Promise, center3Promise, petal3Promise,
-            stem4Promise, center4Promise, petal4Promise,
-            stem5Promise, center5Promise, petal5Promise,
-            stemDPromise, centerDPromise
-        ])
+        Promise.all([bumblebeePromise, grassPromise])
         .then(() => {
             program.init();
             program.initLights();
